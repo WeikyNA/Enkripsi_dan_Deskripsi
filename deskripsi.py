6db3106a84c90c7c1f28704e7d8b716ee9a8cd7c
WeikyNA <123792245+WeikyNA@users.noreply.github.com>
2023-10-27 13:50:18 +0700

    Add files via upload

---
+++ deskripsi.py
@@ -0,0 +1,26 @@
+plaintext = ['ABCDEFGH IJKLMNOPQRSTUVWXYZ.,?!1234567890']
+kata = input('Masukkan Kode: ').upper()
+kunci = int(input('Masukkan Kunci: '))
+ce = []
+m = []
+de = []
+pj = len(kata)
+x = 0
+
+for i in range(pj):
+    m.append(kata[i])
+    ce.append(plaintext[0].find(kata[i]))
+    de.append(plaintext[0].find(kata[i]))
+    x = (ce[i] - kunci) % 41
+    s = ce[i]
+    st = plaintext[0][x]
+
+awal = s
+chiper = x
+plainText = st
+
+import matplotlib.pyplot as plt
+
+plt.plot(x)
+plt.grid(True)
+plt.show()
